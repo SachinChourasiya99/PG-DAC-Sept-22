e8449737a33dfe5d30976403546486f4ee238ed9
SachinChourasiya99 <113438944+SachinChourasiya99@users.noreply.github.com>
2022-09-24 17:28:17 +0530

    Add files via upload

---
+++ Pic2Answer/Pattern20.java
@@ -0,0 +1,15 @@
+class Pattern20{
+ public static void main(String []args){
+  for(int i=4;i>=0;i--){
+   for(int j=0;j<=i;j++){
+    System.out.print(" ");
+   } 
+    for(int k=4;k>=0;k--){
+		System.out.print("*");
+    } 
+	System.out.println();	
+  }  
+  
+  
+ }
+}
