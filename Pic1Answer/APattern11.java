ad95a8a97835837098c120dcd54df27994584001
SachinChourasiya99 <113438944+SachinChourasiya99@users.noreply.github.com>
2022-09-24 17:26:55 +0530

    Add files via upload

---
+++ Pic1Answer/APattern11.java
@@ -0,0 +1,19 @@
+class APattern11{
+ public static void main(String []args){
+  for(int i=1;i<=5;i++){
+   for(int j=i;j<5;j++){
+    System.out.print(" ");
+   } 
+    for(int k=1;k<=i;k++){
+		System.out.print("*");
+    } 
+	 for(int j=1;j<i;j++){
+      System.out.print("*");
+     }
+
+System.out.println();	
+  }  
+  
+  
+ }
+}
---
+++ Pic1Answer/APattern17.java
@@ -0,0 +1,11 @@
+class APattern17{
+ public static void main(String []args){  
+  int a=1;
+  for(int i=1;i<=5;i++){
+   for(int j=0;j<i;j++){
+    System.out.print((a++)+" ");
+   }
+   System.out.println();
+  }
+ }
+}
